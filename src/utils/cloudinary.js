ef892df0ad1924fa1d2ec345af5582b11b053a55
anurag80900 <anuragmauryaraj28@gmail.com>
2025-01-19 15:41:06 +0530

    cloudinary and multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadOnCloudinary = (localFilePath) =>{
+    try {
+        if (!localFilePath) return null
+            //upload file on cloudinary
+        const response=cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        // file has been uploaded successfully
+        console.log("file is uploaded on cloudinary", response.url);
+        return responsse
+        
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //remove the locally saved unlinked file as the upload option got failed
+        return null
+    }
+}
